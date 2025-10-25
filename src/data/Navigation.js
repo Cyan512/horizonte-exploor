0be19f65ebfa940c01b0b6cfafb21cf7cf7eadfd
Cyan512 <182935@unsaac.edu.pe>
2025-10-25 05:56:20 -0500

    upd: package react icons

--- src/data/Navigation.js
+++ src/data/Navigation.js
@@ -22,5 +22,28 @@ export const NAV_LINKS_PAGE = [
 ];
 
 export const NAV_LINKS_CATEGORY = [
-    
+    {
+        id: 1,
+        name: "Trekking y Caminatas",
+        to: "trekking-hiking"
+    },
+    {
+        id: 2,
+        name: "Deportes de Aventura",
+        to: "adventure-sports"
+    },
+    {
+        id: 3,
+        name: "Naturaleza y Aventura",
+        to: "nature-adventure"
+    },
+    {
+        id: 4,
+        name: "Cultura y Arqueología",
+        to: "culture-archaeology"
+    },{
+        id: 5,
+        name: "Experiencias de Aventura",
+        to: "adventure-experiences"
+    }
 ]
--- src/ui/organisms/header/Header.jsx
+++ src/ui/organisms/header/Header.jsx
@@ -1,17 +1,29 @@
-import { NAV_LINKS_PAGE } from "@data/Navigation";
+import { NAV_LINKS_PAGE, NAV_LINKS_CATEGORY } from "@data/Navigation";
 import { Link } from "react-router-dom";
+import { ChevronDown, } from "lucide-react";
+import { useDropdown } from "@hooks/useDropdown";
+import { cn } from "@utils/tailwind-utils";
 
 export default function Header() {
+  const { isOpen, toggle, close } = useDropdown();
   return (
     <header>
-      <div></div>
+      <div>
+        
+      </div>
       <div>
         <div className="max-w-7xl mx-auto">
           <nav className="flex justify-between">
-            {NAV_LINKS_PAGE.map((link_page) => (
-              <Link key={link_page.id} to={link_page.to}>
-                {link_page.name}
-              </Link>
+            {NAV_LINKS_CATEGORY.map((link_category, index) => (
+              <div key={index} className="relative">
+                <button onClick={toggle} className="flex items-center">
+                  {link_category.name}
+                  <ChevronDown  className={cn("", isOpen ? "" : "rotate-180")} />
+                </button>
+                <div className={cn("absolute left-0" , isOpen ? "opacity-0": "opacity-100")}>
+                    12  
+                </div>
+              </div>
             ))}
           </nav>
         </div>
