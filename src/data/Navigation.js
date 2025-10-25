6772e6b681fc55745ccaf05c96c2d8889e63c127
Cyan512 <182935@unsaac.edu.pe>
2025-10-25 02:19:32 -0500

    add: img index

--- src/data/Navigation.js
+++ src/data/Navigation.js
@@ -1,22 +1,26 @@
 export const NAV_LINKS_PAGE = [
-  {
-    id: 1,
-    name: "Home",
-    to: "",
-  },
-  {
-    id: 2,
-    name: "About Us",
-    to: "about",
-  },
-  {
-    id: 3,
-    name: "Destination",
-    to: "destination",
-  },
-  {
-    id: 4,
-    name: "Contact Us",
-    to: "contact",
-  },
+    {
+        id: 1,
+        name: "Home",
+        to: "",
+    },
+    {
+        id: 2,
+        name: "About Us",
+        to: "about",
+    },
+    {
+        id: 3,
+        name: "Destination",
+        to: "destination",
+    },
+    {
+        id: 4,
+        name: "Contact Us",
+        to: "contact",
+    },
 ];
+
+export const NAV_LINKS_CATEGORY = [
+    
+]
